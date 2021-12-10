d2ed4c7bb1e5c9a11ebab7d22846206b37e7c8fd
jkutkut <jorgeregonzalez@gmail.com>
2021-12-10 13:31:07 +0100

    js añadido

---
+++ res/js/inicio/inicio.js
@@ -0,0 +1,11 @@
+
+var typed;
+
+window.addEventListener('load', function() {
+    typed = new Typed(".auto-input", {
+        strings: ["Jodalede", "Diseño Web","Marketing Digital", "Bases de Datos", "Desarrollo De Aplicaciones","El próximo 10"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true
+    });
+});
